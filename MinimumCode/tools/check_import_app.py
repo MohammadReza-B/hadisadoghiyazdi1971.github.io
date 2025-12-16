341812d5f65d5d4a7e7bb93b1586531ba934d55d
Hadi Sadoghi Yazdi <hadisadoghiyazdi1971@gmail.com>
2025-12-16 05:21:28 +0330

    1

--- MinimumCode/tools/check_import_app.py
+++
@@ -1,8 +0,0 @@
-# tools/check_import_app.py
-try:
-    import app
-    print('Imported app module successfully')
-except Exception as e:
-    import traceback
-    traceback.print_exc()
-    print('ERROR:', e)
